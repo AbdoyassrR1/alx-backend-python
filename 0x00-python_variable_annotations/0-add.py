a5d5a960e4f46556388a41366382839e00a5a6dd
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-07-04 15:20:27 +0300

    update

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,7 +2,7 @@
 """contains function that adds two numbers"""
 
 
-def add(a: float, b: float):
+def add(a: float, b: float) -> float:
     """takes two argument a, b and
     return a + b"""
-    return float(a) + float(b)
+    return float(a + b)
